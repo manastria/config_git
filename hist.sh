5acb42c6a8c4e328ff296725ae497421e028bec5
Jean-Philippe DEMORY <jean-philippe.demory@u-picardie.fr>
2017-05-11 14:12:52 +0200

    Supression des anciens fichiers

--- hist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git config --global alias.hist "log --graph --decorate --format=format:'%C(dim red)%h%C(reset) | %C(bold green)%ad%C(reset) | %C(bold magenta)%d%C(reset) %C(reset)%s%C(reset) %C(bold blue)[%aN]%C(reset)' --date=short"
